df06c8d63941f1d8935b4571082058238916540a
faizi67 <126816810+faizi67@users.noreply.github.com>
2023-03-29 21:55:44 +0100

    Update Assignment 2, Statistics and trends, Faizan Ali Khan-1.py

--- Assignment 2, Statistics and trends, Faizan Ali Khan-1.py
+++ Assignment 2, Statistics and trends, Faizan Ali Khan-1.py
@@ -49,4 +49,56 @@ def read_worldbank_data(filename):
     
     return years, countries
 
+def extract_specific_data(indicators, countries, years_data, countries_data):
+    """
+    Extract specific data from the input dataframes based on the provided indicators and countries.
+    
+    Parameters:
+    -----------
+    indicators : list
+        A list of indicators to be extracted.
+    countries : list
+        A list of countries to be extracted.
+    years_data : pd.DataFrame
+        A DataFrame containing the World Bank data with years as columns.
+    countries_data : pd.DataFrame
+        A DataFrame containing the World Bank data with countries as columns.
+    
+    Returns:
+    --------
+    extracted_years_data : pd.DataFrame
+        A DataFrame containing the extracted data from years_data with the specified indicators and countries.
+    extracted_countries_data : pd.DataFrame
+        A DataFrame containing the extracted data from countries_data with the specified indicators and countries.
+    """
+    # Filter the years_data to keep only the specified indicators and countries
+    extracted_years_data = years_data[years_data["Indicator Name"].isin(indicators)]
+    extracted_years_data = extracted_years_data[extracted_years_data["Country Name"].isin(countries)]
+    
+    # Filter the countries_data to keep only the specified indicators and countries
+    extracted_countries_data = countries_data.loc[:, (specific_countries, specific_indicators)]
+    
+    return extracted_years_data, extracted_countries_data
+
+
+# In[2]:
+
+
+# Load the World Bank Climate Change dataset
+years_data, countries_data = read_worldbank_data("API_19_DS2_en_csv_v2_4902199.csv")
+
+
+# In[3]:
+
+
+# Indicators and countries of interest
+specific_countries = ['Russian Federation', 'Japan', 'South Africa', 'Australia', 'Costa Rica']
+specific_indicators = ['Nitrous oxide emissions (thousand metric tons of CO2 equivalent)',
+        'Methane emissions (kt of CO2 equivalent)',
+        'Total greenhouse gas emissions (kt of CO2 equivalent)',
+        'CO2 emissions (kt)']
+
+extracted_years_data, extracted_countries_data = extract_specific_data(specific_indicators, specific_countries, years_data, countries_data)
+
+
   
